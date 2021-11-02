41993fb085286221f4df425bafd0b9e94328b11c
howardtuan <74853361+howardtuan@users.noreply.github.com>
2021-11-02 09:57:39 +0800

    Add files via upload

--- assets/js/course.js
+++ assets/js/course.js
@@ -1,6 +1,6 @@
 $(function(){
 	$('dd[id !="acc1"]').css("width","0px");
-	$("dl a").click(function(){
+	$("dt a").click(function(){
 		if(!$(this).hasClass("currentBtn")){
 		$(".current").animate({"width":"0px"},300);
 		$($(this).attr("href")).animate({"width":"400px"},300);
